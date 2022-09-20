17363daf4e3e0e2ec0670d12135c1a7f69a648ad
Pedro <phptrebil@gmail.com>
2022-09-19 21:28:19 -0300

    Aplicando o RecyclerView

--- app/src/main/java/br/com/alura/ceep/ui/ListaNotasAtivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/ListaNotasAtivity.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 import br.com.alura.ceep.R;
 import br.com.alura.ceep.dao.NotaDAO;
 import br.com.alura.ceep.model.Nota;
-import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
+import br.com.alura.ceep.ui.adapter.RecycleView.ListaNotasAdapter;
 
 public class ListaNotasAtivity extends AppCompatActivity {
 
@@ -18,7 +20,7 @@ public class ListaNotasAtivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
-        ListView lista = findViewById(R.id.listView);
+        RecyclerView lista = findViewById(R.id.RecyclerView);
         NotaDAO dao = new NotaDAO();
         for (int i = 1; i < 10000; i++) {
             dao.insere(new Nota("Primeira nota" + i, "Olá mundo!!!"));
@@ -26,5 +28,7 @@ public class ListaNotasAtivity extends AppCompatActivity {
 
         List<Nota> todos = dao.todos();
         lista.setAdapter(new ListaNotasAdapter(this, todos));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        lista.setLayoutManager(linearLayoutManager);
     }
 }
